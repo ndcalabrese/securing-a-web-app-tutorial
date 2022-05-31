bc32ebe02b06b8216d838380077f18c90c0ee1b8
ndcalabrese <ndcalabrese@gmail.com>
2022-05-31 14:00:35 -0400

    Initial commit

---
+++ src/main/java/com/springtutorial/securingawebapptutorial/SecuringAWebAppTutorialApplication.java
@@ -0,0 +1,13 @@
+package com.springtutorial.securingawebapptutorial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecuringAWebAppTutorialApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SecuringAWebAppTutorialApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/springtutorial/securingawebapptutorial/SecuringAWebAppTutorialApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springtutorial.securingawebapptutorial;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecuringAWebAppTutorialApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
